30c9383040a914b676a45deaac0bc939a63d300b
HanSeongLee <jkhanseong@gmail.com>
2023-03-26 18:02:02 +0900

    FIX | API url

--- lib/apis/ip-geolocation.ts
+++ lib/apis/ip-geolocation.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getIpGeolocation = async () => {
-    const { data } = await axios.get('http://ip-api.com/json/');
+    const { data } = await axios.get('https://ip-api.com/json/');
     return data as IPGeolocation;
 };
