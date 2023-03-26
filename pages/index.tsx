b683089ed508fd77ab63abbaaf50a194bae09011
HanSeongLee <jkhanseong@gmail.com>
2023-03-26 17:53:58 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import styles from './style.module.scss';
+import dynamic from 'next/dynamic';
+
+const WallpaperClockContainer = dynamic(() => import('containers/WallpaperClockContainer'), {
+    ssr: false,
+});
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
-}
+    return (
+        <>
+            <header>
+                <h1 className={styles.hidden}>
+                    Clock App
+                </h1>
+            </header>
+            <main className={styles.main}>
+                <WallpaperClockContainer />
+            </main>
+        </>
+    );
+};
 
 export default Home
